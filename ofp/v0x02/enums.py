3563357b9137a280ec409e676027119648175121
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-03-30 11:34:46 -0300

    Moving OFPType from enums.py to messages.py

--- ofp/v0x02/enums.py
+++ ofp/v0x02/enums.py
@@ -1,49 +1,4 @@
 from enum import Enum
-class OFPType(Enum):
-    """
-    Message Type
-    """
-    # Symetric/Immutable messages
-    OFPT_HELLO = 0
-    OFPT_ERROR = 1
-    OFPT_ECHO_REQUEST = 2
-    OFPT_ECHO_REPLY = 3
-    OFPT_VENDOR = 4
-
-    # Switch configuration messages
-    # Controller/Switch messages
-    OFPT_FEATURES_REQUEST = 5
-    OFPT_FEATURES_REPLY = 6
-    OFPT_GET_CONFIG_REQUEST = 7
-    OFPT_GET_CONFIG_REPLY = 8
-    OFPT_SET_CONFIG = 9
-
-    # Async messages
-    OFPT_PACKET_IN = 10
-    OFPT_FLOW_REMOVED = 11
-    OFPT_PORT_STATUS = 12
-
-    # Controller command messages
-    # Controller/switch message
-    OFPT_PACKET_OUT = 13
-    OFPT_FLOW_MOD = 14
-    OFPT_PORT_MOD = 15
-
-    # Statistics messages
-    # Controller/Switch message
-    OFPT_STATS_REQUEST = 16
-    OFPT_STATS_REPLY = 17
-
-    # Barrier messages
-    # Controller/Switch message
-    OFPT_BARRIER_REQUEST = 18
-    OFPT_BARRIER_REPLY = 19
-
-    # Queue Configuration messages
-    # Controller/Switch message
-    OFPT_QUEUE_GET_CONFIG_REQUEST = 20
-    OFPT_QUEUE_GET_CONFIG_REPLY = 21
-
 
 class OFPPortConfig(Enum):
     """
