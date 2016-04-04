ea6151e76452745b170c9d8a80e2c0279a0be673
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-04-04 14:28:23 -0300

    Added header class and its requirements

--- ofp/v0x02/common_structures/header.py
+++ ofp/v0x02/common_structures/header.py
@@ -1,11 +1,81 @@
-from common.basic_types import UBInt8, UBInt16, UBInt32
-from common.base import GenericStruct
+"""Defines Header classes and related items"""
 
-class OFPHeader(GenericStruct):
-    version = UBInt8()
-    ofp_type = UBInt8()
-    length = UBInt16()
-    xid = UBInt32()
+# System imports
+import enum
+
+# Third-party imports
+
+# Local source tree imports
+from common import base
+from common import basic_types
 
 
+class OFPType(enum.Enum):
+    """
+    Message Type
+    """
+    # Symetric/Immutable messages
+    OFPT_HELLO = 0
+    OFPT_ERROR = 1
+    OFPT_ECHO_REQUEST = 2
+    OFPT_ECHO_REPLY = 3
+    OFPT_VENDOR = 4
+
+    # Switch configuration messages
+    # Controller/Switch messages
+    OFPT_FEATURES_REQUEST = 5
+    OFPT_FEATURES_REPLY = 6
+    OFPT_GET_CONFIG_REQUEST = 7
+    OFPT_GET_CONFIG_REPLY = 8
+    OFPT_SET_CONFIG = 9
+
+    # Async messages
+    OFPT_PACKET_IN = 10
+    OFPT_FLOW_REMOVED = 11
+    OFPT_PORT_STATUS = 12
+
+    # Controller command messages
+    # Controller/switch message
+    OFPT_PACKET_OUT = 13
+    OFPT_FLOW_MOD = 14
+    OFPT_PORT_MOD = 15
+
+    # Statistics messages
+    # Controller/Switch message
+    OFPT_STATS_REQUEST = 16
+    OFPT_STATS_REPLY = 17
+
+    # Barrier messages
+    # Controller/Switch message
+    OFPT_BARRIER_REQUEST = 18
+    OFPT_BARRIER_REPLY = 19
+
+    # Queue Configuration messages
+    # Controller/Switch message
+    OFPT_QUEUE_GET_CONFIG_REQUEST = 20
+    OFPT_QUEUE_GET_CONFIG_REPLY = 21
+
+
+class OFPHeader(base.GenericStruct):
+    """Representation of an OpenFlow message Header
+
+    :param version: version of the Protocol
+    :param ofp_type: Type of  the message
+    :param length: Length of the message, including the header itself
+    :param xid: id of the message
+    """
+    version = basic_types.UBInt8()
+    ofp_type = basic_types.UBInt8()
+    length = basic_types.UBInt16()
+    xid = basic_types.UBInt32()
+
+    def __init__(self,
+                 version=base.OFP_VERSION,
+                 ofp_type=OFPType.OFPT_HELLO,
+                 length=0,
+                 xid=None):
+        self.version = version
+        self.ofp_type = ofp_type
+        self.length = length
+        self.xid = xid
 
