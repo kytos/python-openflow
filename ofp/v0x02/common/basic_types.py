8d20b3ef6b384c8f0fa12aa523cb33c07daddd65
Artur Baruchi <artur.baruchi@gmail.com>
2016-04-04 14:23:16 -0300

    Added Doc String to classes and break imports according to good practices.

--- ofp/v0x02/common/basic_types.py
+++ ofp/v0x02/common/basic_types.py
@@ -1,23 +1,39 @@
-from struct import pack, unpack_from, calcsize, error
-from utils.exceptions import OFPException
-from common.base import GenericType
+from struct import pack
+from struct import unpack
+from struct import unpack_from
+from common import base
+from common import exceptions
+
 
 # TODO: Refactor unpack methods to return the unpacked object
 #       instead of being an inplace method.
 
-class UBInt8(GenericType):
+
+class UBInt8(base.GenericType):
+    """
+    Format character for an Unsigned Char. Class for an 8 bytes
+    Unsigned Integer.
+    """
     fmt = "!B"
 
 
-class UBInt8Array(GenericType):
-    def __init__(self, value = None, length = 0):
+class UBInt8Array(base.GenericType):
+    """
+    Creates an Array of Unsigned Integer of 8 bytes.
+    """
+    def __init__(self, value=None, length=0):
         if value:
             self.value = value
         self.length = length
         self.fmt = "!%d%c" % (self.length, 'B')
 
     def unpack(self, buff, offset=0):
-        """ Unpack a buff and stores at value property. """
+        """
+        Unpack a buff and stores at value property.
+        Keyword arguments:
+            buff -- Buffer where data is located.
+            offset -- Where data stream begins.
+        """
         self.value = unpack_from(self.fmt, buff, offset)
 
     def pack(self):
