6905c6982f78e3a4d64a0d4d92ae3e9ebba78320
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-04-05 17:40:00 -0300

    adding get_config_request class

---
+++ ofp/v0x02/controller_to_switch/get_config_request.py
@@ -0,0 +1,17 @@
+"""Defines Get Config Request classes and related items"""
+
+# Local source tree imports
+from common import base
+from common_structures import header as of_header
+
+class GetConfigRequest(base.GenericStruct):
+    """Get Config Request message.
+
+    This message does not contain a body beyond the OpenFlow Header
+        :param xid: xid to be used on the message header
+    """
+    header = of_header.OFPHeader()
+
+    def __init__(self, xid=None):
+        self.header.ofp_type = of_header.OFPType.OFPT_GET_CONFIG_REQUEST
+        self.header.xid = xid
