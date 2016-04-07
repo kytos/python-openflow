360993d4184dd26d077c06dd5839e6d6c46f404d
Artur Baruchi <artur.baruchi@gmail.com>
2016-04-07 15:16:29 -0300

    Add constructor for the class OFPPort

--- ofp/v0x02/common/port.py
+++ ofp/v0x02/common/port.py
@@ -191,3 +191,28 @@ class OFPPort(base.GenericStruct):
 
     curr_speed = basic_types.UBInt32()
     max_speed = basic_types.UBInt32()
+
+    def __init__(self,
+                 port_no=None,
+                 hw_addr=None,
+                 name=None,
+                 config=None,
+                 state=None,
+                 curr=None,
+                 advertised=None,
+                 supported=None,
+                 peer=None,
+                 curr_speed=None,
+                 max_speed=None):
+
+        self.port_no = port_no
+        self.hw_addr = hw_addr
+        self.name = name
+        self.config = config
+        self.state = state
+        self.curr = curr
+        self.advertised = advertised
+        self.supported = supported
+        self.peer = peer
+        self.curr_speed = curr_speed
+        self.max_speed = max_speed
