3557f76b99230b2d84e17429e766343c3dc41d27
Artur Baruchi <artur.baruchi@gmail.com>
2016-04-11 15:37:39 -0300

    Refactor features_reply according to version 1.0.0

--- ofp/v0x01/controller2switch/features_reply.py
+++ ofp/v0x01/controller2switch/features_reply.py
@@ -7,6 +7,7 @@ import enum
 
 # Local source tree imports
 from common import header as of_header
+from common import port
 from foundation import base
 from foundation import basic_types
 
@@ -17,19 +18,16 @@ from foundation import basic_types
 class Capabilities(enum.Enum):
     """Enumeration of Capabilities supported by the datapath
 
-        OFPC_FLOW_STATS   # Flow statistics
-        OFPC_TABLE_STATS  # Table statistics
-        OFPC_PORT_STATS   # Port statistics
-        OFPC_GROUP_STATS  # Group statistics
-        OFPC_IP_REASM     # Can reassemble IP fragments
-        OFPC_QUEUE_STATS  # Queue statistics
-        OFPC_ARP_MATCH_IP # Match IP addresses in ARP pkts
+    Enums:
+
+
     """
-    OFPC_FLOW_STATS = 1 << 0
+    OFPC_FLOW_STATS = 1 << 0    # Flow statistics
     OFPC_TABLE_STATS = 1 << 1   # Table statistics
     OFPC_PORT_STATS = 1 << 2    # Port statistics
-    OFPC_GROUP_STATS = 1 << 3   # Group statistics
-    OFPC_IP_REASM = 1 << 5      # Can reassemble IP fragments
+    OFPC_STP = 1 << 3           # 802.1d spanning tree
+    OFPC_RESERVED = 1 << 4      # Reserved, must be zero
+    OFPC_IP_REASM = 1 << 5      # Can reassembe IP fragments
     OFPC_QUEUE_STATS = 1 << 6   # Queue statistics
     OFPC_ARP_MATCH_IP = 1 << 7  # Match IP addresses in ARP pkts
 
@@ -57,13 +55,14 @@ class SwitchFeatures(base.GenericStruct):
     """
 
     header = of_header.OFPHeader()
-    datapath_id = basic_types.UBInt64
-    n_buffers = basic_types.UBInt32
-    n_tables = basic_types.UBInt8
+    datapath_id = basic_types.UBInt64()
+    n_buffers = basic_types.UBInt32()
+    n_tables = basic_types.UBInt8()
     pad = basic_types.UBInt8Array(length=3) # Align to 64-bits
     # Features
-    capabilities = basic_types.UBInt32
-    reserved = basic_types.UBInt32
+    capabilities = basic_types.UBInt32()
+    actions = basic_types.UBInt32()
+    reserved = basic_types.UBInt32()
 
     # Port info
     # TODO: On the official spec this element acts like a pointer to the first
@@ -72,19 +71,13 @@ class SwitchFeatures(base.GenericStruct):
     #       In the case of using a pointer, should we create a class that
     #       represents a list of ports and then point to an instance of it?
     #       We should consider that in Python all objects acts as C++ pointers
-    # https://rg03.wordpress.com/2007/04/21/semantics-of-python-variable-names-from-a-c-perspective/
-    ports = None # The number of ports is inferred
+    #       https://goo.gl/p1nzxh
+    ports = port.Port()     # The number of ports is inferred
                             # from the length field in the header
 
-    def __init__(self,
-                 xid=None,
-                 datapath_id=None,
-                 n_buffers=0,
-                 ntables=0,
-                 pad=0,
-                 capabilities=0,
-                 reserved=0,
-                 ports=None):
+    def __init__(self, xid=None, datapath_id=None, n_buffers=0, ntables=0,
+                 pad=0, capabilities=0, reserved=0, ports=None):
+
         self.header.ofp_type = of_header.OFPType.OFPT_FEATURES_REPLY
         self.header.xid = xid
         self.datapath_id = datapath_id
