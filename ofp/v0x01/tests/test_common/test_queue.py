099a908f19d597aec70b1fac6131e7ebb75fa79a
Andre Tadeu de Carvalho <andretadeu@ncc.unesp.br>
2016-04-13 18:12:24 -0300

    Fix test.

--- ofp/v0x01/tests/test_common/test_queue.py
+++ ofp/v0x01/tests/test_common/test_queue.py
@@ -5,21 +5,6 @@ import os
 from ofp.v0x01.common import queue
 from ofp.v0x01.foundation import basic_types
 
-
-class TestPacketQueue(unittest.TestCase):
-
-    def test_get_size(self):
-        packet_queue = queue.PacketQueue(1, 8, [0, 0])
-        self.assertEqual(packet_queue.get_size(), 8)
-
-    def test_pack(self):
-        packet_queue = queue.PacketQueue(1, 8, [0, 0])
-        packet_queue.pack()
-
-    def test_unpack(self):
-        pass
-
-
 class TestQueuePropHeader(unittest.TestCase):
 
     def test_get_size(self):
@@ -33,6 +18,20 @@ class TestQueuePropHeader(unittest.TestCase):
     def test_unpack(self):
         pass
 
+class TestPacketQueue(unittest.TestCase):
+
+    def test_get_size(self):
+        queue_prop_header = queue.QueuePropHeader(1, 8, [1, 1, 1, 1])
+        packet_queue = queue.PacketQueue(1, 8, [0, 0], queue_prop_header)
+        self.assertEqual(packet_queue.get_size(), 8)
+
+    def test_pack(self):
+        queue_prop_header = queue.QueuePropHeader(1, 8, [1, 1, 1, 1])
+        packet_queue = queue.PacketQueue(1, 8, [0, 0], queue_prop_header)
+        packet_queue.pack()
+
+    def test_unpack(self):
+        pass
 
 class TestQueuePropMinRate(unittest.TestCase):
 
