bcc27885213a58951ef784213b2b45d6263b1c15
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-04-18 16:56:31 -0300

    Fix typo on setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='OpenFlow Next Generation
+setup(name='OpenFlow Next Generation',
       version='0.1',
       description='Library to parse and generate messages based on the OpenFlow Protocol',
       url='http://github.com/of-ng/ofx-parser',
