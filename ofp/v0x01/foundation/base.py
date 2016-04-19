527dd24e92c8e3f509e277b36344d9558305019f
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-04-19 14:42:03 -0300

    [v0x01] GenericType pack exception improvement

--- ofp/v0x01/foundation/base.py
+++ ofp/v0x01/foundation/base.py
@@ -76,7 +76,7 @@ class GenericType(object):
         except struct.error:
             message = "Value out of the possible range to basic type "
             message = message + self.__class__.__name__
-            raise exceptions.OFPBadValueException(message)
+            raise exceptions.OFPBadValueException(message) from struct.error
 
     def unpack(self, buff, offset=0):
         """ Unpack a buff and stores at _value property. """
