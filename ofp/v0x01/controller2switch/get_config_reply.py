568ac559996091140edd72408478c7c59a5496cc
Artur Baruchi <abaruchi@ncc.unesp.br>
2016-05-03 14:39:03 -0300

    Rewrite GetConfigReply classe to be a subclass of SwitchConfig class - Issue #25

--- ofp/v0x01/controller2switch/get_config_reply.py
+++ ofp/v0x01/controller2switch/get_config_reply.py
@@ -1,66 +1,16 @@
-"""Defines Features Reply classes and related items"""
+"""Defines GetConfigReply message"""
 
 # System imports
-import enum
 
 # Third-party imports
 
 # Local source tree imports
 from ofp.v0x01.common import header as of_header
-from ofp.v0x01.foundation import base
-from ofp.v0x01.foundation import basic_types
+from ofp.v0x01.controller2switch import common
 
 
-# Enums
+class GetConfigReply(common.SwitchConfig):
 
-
-class ConfigFlags(enum.Enum):
-    """Configuration Flags
-
-        # Handling of IP Fragments
-        OFPC_FRAG_NORMAL         # No special handling for fragments
-        OFPC_FRAG_DROP           # Drop fragments
-        OFPC_FRAG_REASM          # Reassemble (only if OFPC_IP_REASM set)
-        OFPC_FRAG_MASK
-    """
-    OFPC_FRAG_NORMAL = 0
-    OFPC_FRAG_DROP = 1
-    OFPC_FRAG_REASM = 2
-    OFPC_FRAG_MASK = 3
-
-
-# Classes
-
-# TODO: Issue #4
-class SwitchConfig(base.GenericStruct):
-    """Message used on OFPT_SET_CONFIG and OFPT_GET_CONFIG_REPLY.
-
-    This is OFPT_GET_CONFIG_REPLY message sent by the switch to
-    the controller in response to the OFPT_GET_CONFIG_REQUEST,
-    and also is the OFPT_SET_CONFIG message sent by the controller
-    to the switch.
-
-        :param xid:           xid to be used on the message header
-        :param set_message:   Boolean to define if the message is
-                              a SET_CONFIG or GET_CONFIG_REPLY.
-                              - True (default): OFPT_SET_CONFIG
-                              - False: OFPT_GET_CONFIG_REPLY
-        :param flags:         UBInt16 OFPC_* flags
-        :param miss_send_len: UBInt16 max bytes of new flow that the
-                              datapath should send to the controller
-    """
-
-    header = of_header.Header()
-    flags = basic_types.UBInt16()
-    miss_send_len = basic_types.UBInt16()
-
-    def __init__(self, xid=None, set_message=True, flags=None,
-                 miss_send_len=None):
-
-        if set_message:
-            self.header.message_type = of_header.Type.OFPT_SET_CONFIG
-        else:
-            self.header.message_type = of_header.Type.OFPT_GET_CONFIG_REPLY
-        self.header.xid = xid
-        self.flags = flags
-        self.miss_send_len = miss_send_len
+    def __init__(self, xid=None, flags=None, miss_send_len=None):
+        common.SwitchConfig.__init__(self, xid, flags, miss_send_len)
+        self.header.message_type = of_header.Type.OFPT_GET_CONFIG_REPLY
