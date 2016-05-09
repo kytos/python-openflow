fdf3e9f24beb6bc6ab9b3a4cd827d8fbcccb1405
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-05-09 16:57:49 -0300

    [v0x01][Tests] Adding tests for SET_CONFIG and GET_CONFIG_REPLY

--- tests/v0x01/test_controller2switch/test_set_config.py
+++ tests/v0x01/test_controller2switch/test_set_config.py
@@ -1,29 +1,29 @@
 import unittest
 
 from pyof.v0x01.controller2switch import flow_mod
-from pyof.v0x01.controller2switch import get_config_reply
+from pyof.v0x01.controller2switch import set_config
 
 
-class TestGetConfigReply(unittest.TestCase):
+class TestSetConfig(unittest.TestCase):
 
     def setUp(self):
-        self.message = get_config_reply.GetConfigReply()
+        self.message = set_config.SetConfig()
         self.message.header.xid = 1
         self.message.flags = flow_mod.FlowModFlags.OFPFF_EMERG
         self.message.miss_send_len = 1024
 
     def test_get_size(self):
-        """[Controller2Switch/GetConfigReply] - size 12"""
+        """[Controller2Switch/SetConfig] - size 12"""
         self.assertEqual(self.message.get_size(), 12)
 
     @unittest.skip('Not yet implemented')
     def test_pack(self):
-        """[Controller2Switch/GetConfigReply] - packing"""
+        """[Controller2Switch/SetConfig] - packing"""
         # TODO
         pass
 
     @unittest.skip('Not yet implemented')
     def test_unpack(self):
-        """[Controller2Switch/GetConfigReply] - unpacking"""
+        """[Controller2Switch/SetConfig] - unpacking"""
         # TODO
         pass
