d8f5b3ddc059e8f142c1f48e113c1bf6d52b7529
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-05-18 13:49:57 -0300

    FIX #16 and FIX #17
     - Creates the "validate" method on GenericStruct
     - Creates the "_validate_attributes_type" method
     - Call validate on pack
     - Creates a new Exception (AttributeTypeError)

--- pyof/v0x01/foundation/exceptions.py
+++ pyof/v0x01/foundation/exceptions.py
@@ -19,3 +19,18 @@ class PADHasNoValue(Exception):
     """
     def __str__(self):
         return "You can't set a value on a PAD attribute"
+
+
+class AttributeTypeError(Exception):
+    """Error raise when the attribute is not of the expected type
+    defined on the class definition"""
+
+    def __init__(self, item, item_class, expected_class):
+        self.item = item
+        self.item_class = item_class
+        self.expected_class = expected_class
+
+    def __str__(self):
+        return ("Unexpected value ('{}') on attribute '{}' "
+                "from class '{}'".format(self.item,
+                                         self.name, self.expected_class))
