e20d404b6140210cdb31f3317504490d54601b22
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-06-13 12:58:07 -0300

    Added pep8 check on python setup.py test

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ import os
 import sys
 from setuptools import setup, find_packages, Command
 
+SEP='<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>'
+
 
 class Doctest(Command):
     if sys.argv[-1] == 'test':
+        print(SEP)
         print("Running docs make and make doctest")
         os.system("make doctest -C docs/")
+        print(SEP)
+
+
+class Pep8Test(Command):
+    if sys.argv[-1] == 'test':
+        print("Running pep8 under source code folder")
+        os.system("python setup.py pep8 --exclude '.eggs*'")
+        print(SEP)
 
 setup(name='Kytos OpenFlow Parser library',
       version='0.1',
@@ -17,8 +28,8 @@ setup(name='Kytos OpenFlow Parser library',
       license='MIT',
       test_suite='tests',
       packages=find_packages(exclude=["tests", "*v0x02*"]),
+      setup_requires=['setuptools-pep8'],
       cmdclass={
         'doctests': Doctest
       },
       zip_safe=False)
-
