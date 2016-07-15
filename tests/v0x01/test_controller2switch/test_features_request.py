af615f4f9d551ca221de5231694f31a6e67217e8
Carlos Eduardo Moreira dos Santos <cadu@ncc.unesp.br>
2016-07-15 20:40:25 -0300

    Fixing pep8 issues in tests
    Close #116

--- tests/v0x01/test_controller2switch/test_features_request.py
+++ tests/v0x01/test_controller2switch/test_features_request.py
@@ -4,6 +4,7 @@ import os
 from pyof.v0x01.controller2switch import features_request
 from pyof.v0x01.common import header as of_header
 
+
 class TestFeaturesRequest(unittest.TestCase):
 
     def setUp(self):
