8f3dd5a9a0c3311ee347e3afda8a0f22aba85fa7
Carlos Eduardo Moreira dos Santos <cadu@ncc.unesp.br>
2016-08-17 16:41:14 -0300

    SetConfig raw dump test

--- tests/v0x01/test_controller2switch/test_set_config.py
+++ tests/v0x01/test_controller2switch/test_set_config.py
@@ -1,7 +1,5 @@
 """Set Config message tests."""
-import unittest
-
-from pyof.v0x01.controller2switch.flow_mod import FlowModFlags
+from pyof.v0x01.controller2switch.common import ConfigFlags
 from pyof.v0x01.controller2switch.set_config import SetConfig
 from tests.teststruct import TestStruct
 
@@ -14,15 +12,7 @@ class TestSetConfig(TestStruct):
         """Configure raw file and its object in parent class (TestDump)."""
         super().setUpClass()
         super().set_raw_dump_file('v0x01', 'ofpt_set_config')
-        super().set_raw_dump_object(SetConfig, xid=1,
-                                    flags=FlowModFlags.OFPFF_EMERG,
-                                    miss_send_len=1024)
+        super().set_raw_dump_object(SetConfig, xid=3,
+                                    flags=ConfigFlags.OFPC_FRAG_NORMAL,
+                                    miss_send_len=128)
         super().set_minimum_size(12)
-
-    @unittest.skip('Need to recover dump contents.')
-    def test_pack(self):
-        pass
-
-    @unittest.skip('Need to recover dump contents.')
-    def test_unpack(self):
-        pass
