7f38b4e848ccb8dddf3450ed4c9bd9f2a66232af
Artur Baruchi <artur.baruchi@gmail.com>
2016-09-14 11:05:17 -0300

    Added unit tests for QueueStats and QueueStatsRequest - Issue #158

---
+++ tests/v0x04/test_controller2switch/test_queue_stats_request.py
@@ -0,0 +1,15 @@
+"""Queue Stat Request message."""
+from pyof.v0x04.controller2switch.common import QueueStatsRequest
+from tests.test_struct import TestStruct
+
+
+class TestQueueStatsRequest(TestStruct):
+    """Queue Stat Request message."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Configure raw file and its object in parent class (TestDump)."""
+        super().setUpClass()
+        super().set_raw_dump_file('v0x04', 'ofpt_queue_stats_request')
+        super().set_raw_dump_object(QueueStatsRequest)
+        super().set_minimum_size(8)
