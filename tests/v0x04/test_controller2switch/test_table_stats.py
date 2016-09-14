8d9603e2206226b581edf34c53242033d1473067
Artur Baruchi <artur.baruchi@gmail.com>
2016-09-14 16:45:32 -0300

    Added unit test for TableStat classe - Issue #158

---
+++ tests/v0x04/test_controller2switch/test_table_stats.py
@@ -0,0 +1,15 @@
+"""Table stats message."""
+from pyof.v0x04.controller2switch.common import TableStats
+from tests.test_struct import TestStruct
+
+
+class TestTableStats(TestStruct):
+    """Table stats message."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Configure raw file and its object in parent class (TestDump)."""
+        super().setUpClass()
+        super().set_raw_dump_file('v0x04', 'ofpt_table_stats')
+        super().set_raw_dump_object(TableStats)
+        super().set_minimum_size(24)
