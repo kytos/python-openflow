1034ee859c6438e0ac209bb0f06b709b00a55476
Beraldo Leal <beraldo.ncc@gmail.com>
2016-09-30 16:58:13 -0300

    added hash and data to Ethernet

--- pyof/foundation/basic_types.py
+++ pyof/foundation/basic_types.py
@@ -557,9 +557,17 @@ class Ethernet(GenericStruct):
     destination = HWAddress()
     source = HWAddress()
     type = UBInt16()
+    data = BinaryData()
 
-    def __init__(self, destination=None, source=None, type=None):
+    def __init__(self, destination=None, source=None, type=None, data=b''):
         super().__init__()
         self.destination = destination
         self.source = source
         self.type = type
+        self.data = data
+
+    def __hash__(self):
+        return hash(self.pack())
+
+    def get_hash(self):
+        return self.__hash__()
