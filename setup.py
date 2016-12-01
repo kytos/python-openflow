2b4e3742bb72fe5b5966ebc4003bbb91ffe6fca7
Carlos Eduardo Moreira dos Santos <cadu@ncc.unesp.br>
2016-11-30 22:05:31 -0200

    Update linters

--- setup.py
+++ setup.py
@@ -3,87 +3,43 @@
 Run "python3 setup --help-commands" to list all available commands and their
 descriptions.
 """
-import os
 import sys
+from distutils.command.build import build
 from subprocess import call, check_call
-from setuptools import Command, find_packages, setup
+
+from setuptools import find_packages, setup
 
 from pyof import __version__
 
 
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-
-    user_options = []
-
-    def initialize_options(self):
-        """No initializa options."""
-        pass
-
-    def finalize_options(self):
-        """No finalize options."""
-        pass
-
-    def run(self):
-        """Clean build, dist, pyc and egg from package and docs."""
-        os.system('rm -vrf ./build ./dist ./*.pyc ./*.egg-info')
-        os.system('cd docs; make clean')
-
-
-class Doctest(Command):
-    """Run Sphinx doctest."""
-
-    if sys.argv[-1] == 'test':
-        print('Running examples in documentation')
-        check_call('make doctest -C docs/', shell=True)
-
-
-class Linter(Command):
-    """Run several code linters."""
-
-    description = 'Run many code linters. It may take a while'
-    user_options = []
-
-    def __init__(self, *args, **kwargs):
-        """Define linters and a message about them."""
-        super().__init__(*args, **kwargs)
-        self.linters = ['pep257', 'pyflakes', 'mccabe', 'isort', 'pep8',
-                        'pylint']
-        self.extra_msg = 'It may take a while. For a faster version (and ' \
-                         'less checks), run "quick_lint".'
-
-    def initialize_options(self):
-        """For now, options are ignored."""
-        pass
-
-    def finalize_options(self):
-        """For now, options are ignored."""
-        pass
+class Linter(build):
+    """Code linters."""
 
     def run(self):
         """Run pylama and radon."""
         files = 'tests setup.py pyof'
-        print('running pylama with {}. {}'.format(', '.join(self.linters),
-                                                  self.extra_msg))
-        cmd = 'pylama -l {} {}'.format(','.join(self.linters), files)
+        print('Running pylama. It may take a while...')
+        cmd = 'pylama {}'.format(files)
         call(cmd, shell=True)
-        print('Low grades (<= C) for Cyclomatic Complexity:')
-        call('radon cc --min=C ' + files, shell=True)
         print('Low grades (<= C) for Maintainability Index:')
         call('radon mi --min=C ' + files, shell=True)
 
 
-class FastLinter(Linter):
-    """Same as Linter, but without the slow pylint."""
+class Cleaner(build):
+    """Custom clean command to tidy up the project root."""
 
-    description = 'Same as "lint", but much faster (no pylama_pylint).'
+    def run(self):
+        """Clean build, dist, pyc and egg from package and docs."""
+        call('rm -vrf ./build ./dist ./*.pyc ./*.egg-info', shell=True)
+        call('cd docs; make clean', shell=True)
 
-    def __init__(self, *args, **kwargs):
-        """Remove slow linters and redefine the message about the rest."""
-        super().__init__(*args, **kwargs)
-        self.linters.remove('pylint')
-        self.extra_msg = 'This a faster version of "lint", without pylint. ' \
-                         'Run the slower "lint" after solving these issues:'
+
+class Doctest(build):
+    """Run Sphinx doctest during test."""
+
+    if sys.argv[-1] == 'test':
+        print('Running examples in documentation')
+        check_call('make doctest -C docs/', shell=True)
 
 
 setup(name='python-openflow',
@@ -94,10 +50,9 @@ setup(name='python-openflow',
       author_email='of-ng-dev@ncc.unesp.br',
       license='MIT',
       test_suite='tests',
-      packages=find_packages(exclude=['tests', '*v0x02*']),
+      packages=find_packages(exclude=['tests', '*v0x02*', '*v0x04*']),
       cmdclass={
           'lint': Linter,
-          'quick_lint': FastLinter,
-          'clean': CleanCommand,
+          'clean': Cleaner,
       },
       zip_safe=False)
