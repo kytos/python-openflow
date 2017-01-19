97dc99145fb892607ab208a74e6d079eab4fbd0a
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-01-19 07:07:50 -0200

    Fixing inheritance test.
    It was fixed to work without the "rename(d)_attributes" and
    "insert(ed)_attributes_before" class helpers, these behaviors will may
    be added on the future, but, for now, we are not going to consider them.

--- tests/test_class_inheritance/test_inheritance.py
+++ tests/test_class_inheritance/test_inheritance.py
@@ -27,44 +27,45 @@ class TestInheritance(unittest.TestCase):
             a2 = UBInt64(2)
             b6 = UBInt8(6)
 
-            _remove_attributes = ['a3']
-            _rename_attributes = [('a4', 'b4')]
-            _insert_attributes_before = {'a0': 'a1'}
+            _removed_attributes = ['a3']
+            # _rename_attributes = [('a4', 'b4')]
+            # _insert_attributes_before = {'a0': 'a1'}
 
         self.MyClassA = MyClassA
         self.MyClassB = MyClassB
         self.a_expected_names = ['a1', 'a2', 'a3', 'a4', 'a5']
-        self.b_expected_names = ['a0', 'a1', 'a2', 'b4', 'a5', 'b6']
+        self.b_expected_names = ['a1', 'a2', 'a4', 'a5', 'a0', 'b6']
+        # self.b_expected_names = ['a0', 'a1', 'a2', 'b4', 'a5', 'b6']
 
     def test_modifications(self):
         """[Foundation/Base/GenericStruct] - Attributes Modifications."""
+        m1 = self.MyClassA()
+        m2 = self.MyClassB()
         # Checking keys (attributes names) and its ordering
-        self.assertEqual(list(self.MyClassA.__ordered__.keys()),
+        self.assertEqual([attr[0] for attr in m1._get_class_attributes()],
                          self.a_expected_names)
-        self.assertEqual(list(self.MyClassB.__ordered__.keys()),
+        self.assertEqual([attr[0] for attr in m2._get_class_attributes()],
                          self.b_expected_names)
 
         # Check if there is no shared attribute between instances
-        m1 = self.MyClassA()
-        m2 = self.MyClassB()
         self.assertIsNot(m1, m2)
-        self.assertIsNot(m1.a1, m2.a0)
         self.assertIsNot(m1.a1, m2.a1)
         self.assertIsNot(m1.a2, m2.a2)
-        self.assertIsNot(m1.a4, m2.b4)
+        self.assertIsNot(m1.a3, m2.a4)
+        self.assertIsNot(m1.a4, m2.a4)
         self.assertIsNot(m1.a5, m2.a5)
 
         # Check attributes types on MyClassA
-        self.assertIs(self.MyClassA.__ordered__['a1'], UBInt8)
-        self.assertIs(self.MyClassA.__ordered__['a2'], UBInt16)
-        self.assertIs(self.MyClassA.__ordered__['a3'], UBInt8)
-        self.assertIs(self.MyClassA.__ordered__['a4'], UBInt16)
-        self.assertIs(self.MyClassA.__ordered__['a5'], UBInt32)
+        self.assertIsInstance(self.MyClassA.a1, UBInt8)
+        self.assertIsInstance(self.MyClassA.a2, UBInt16)
+        self.assertIsInstance(self.MyClassA.a3, UBInt8)
+        self.assertIsInstance(self.MyClassA.a4, UBInt16)
+        self.assertIsInstance(self.MyClassA.a5, UBInt32)
 
         # Check attributes types on MyClassA
-        self.assertIs(self.MyClassB.__ordered__['a0'], UBInt32)
-        self.assertIs(self.MyClassB.__ordered__['a1'], UBInt8)
-        self.assertIs(self.MyClassB.__ordered__['a2'], UBInt64)
-        self.assertIs(self.MyClassB.__ordered__['b4'], UBInt16)
-        self.assertIs(self.MyClassB.__ordered__['a5'], UBInt32)
-        self.assertIs(self.MyClassB.__ordered__['b6'], UBInt8)
+        self.assertIsInstance(self.MyClassB.a1, UBInt8)
+        self.assertIsInstance(self.MyClassB.a2, UBInt64)
+        self.assertIsInstance(self.MyClassB.a4, UBInt16)
+        self.assertIsInstance(self.MyClassB.a5, UBInt32)
+        self.assertIsInstance(self.MyClassB.a0, UBInt32)
+        self.assertIsInstance(self.MyClassB.b6, UBInt8)
