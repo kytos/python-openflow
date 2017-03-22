abca8beba0dc1bdd2362b622f005331d5a33d063
Macartur Sousa <macartur.sc@gmail.com>
2017-03-22 13:52:14 -0300

    Adding some tests for multipart request/reply

--- tests/v0x04/test_controller2switch/test_multipart_reply.py
+++ tests/v0x04/test_controller2switch/test_multipart_reply.py
@@ -1,9 +1,8 @@
 """MultipartReply message test."""
 
 from pyof.v0x04.controller2switch.common import MultipartTypes
-from pyof.v0x04.controller2switch.multipart_reply import (MultipartReply,
+from pyof.v0x04.controller2switch.multipart_reply import (Desc, MultipartReply,
                                                           MultipartReplyFlags)
-
 from tests.v0x04.test_struct import TestStruct
 
 
@@ -19,3 +18,22 @@ class TestMultipartReply(TestStruct):
                             flags=MultipartReplyFlags.OFPMPF_REPLY_MORE,
                             body='')
         super().set_minimum_size(16)
+
+    @staticmethod
+    def get_attributes(multipart_type=MultipartTypes.OFPMP_DESC,
+                       flags=MultipartReplyFlags.OFPMPF_REPLY_MORE,
+                       body=''):
+        """Method used to return a dict with instance paramenters."""
+        return {'xid': 32, 'multipart_type': multipart_type, 'flags': flags,
+                'body': body}
+
+    def test_pack_unpack_desc(self):
+        """Testing multipart_type with OFPMP_DESC."""
+        instances = Desc(mfr_desc="MANUFACTURER DESCRIPTION",
+                         hw_desc="HARDWARE DESCRIPTION",
+                         sw_desc="SOFTWARE DESCRIPTION",
+                         serial_num="SERIAL NUMBER",
+                         dp_desc="DATAPATH DESCRIPTION")
+        options = TestMultipartReply.get_attributes(
+            multipart_type=MultipartTypes.OFPMP_DESC, body=instances)
+        self._test_pack_unpack(**options)
--- tests/v0x04/test_controller2switch/test_multipart_request.py
+++ tests/v0x04/test_controller2switch/test_multipart_request.py
@@ -1,6 +1,7 @@
 """MultipartRequest message test."""
 from pyof.v0x04.controller2switch.multipart_request import (
-    MultipartRequest, MultipartRequestFlags, MultipartTypes, TableFeatures)
+    MultipartRequest, MultipartRequestFlags, MultipartTypes,
+    PortStatsRequest, TableFeatures)
 
 from tests.v0x04.test_struct import TestStruct
 
@@ -19,14 +20,45 @@ class TestMultipartRequest(TestStruct):
                             body=b'')
         super().set_minimum_size(16)
 
-    def test_body_table_features(self):
-        """Method to test pack and unpack using table_features as body."""
-        table_features = [TableFeatures(table_id=2),
-                          TableFeatures(table_id=6),
-                          TableFeatures(table_id=4)]
+    @staticmethod
+    def get_attributes(multipart_type=MultipartTypes.OFPMP_DESC,
+                       flags=MultipartRequestFlags.OFPMPF_REQ_MORE,
+                       body=''):
+        """Method used to return a dict with instance paramenters."""
+        return {'xid': 32, 'multipart_type': multipart_type, 'flags': flags,
+                'body': body}
 
-        options = {'xid': 16,
-                   'multipart_type': MultipartTypes.OFPMP_TABLE_FEATURES,
-                   'flags': MultipartRequestFlags.OFPMPF_REQ_MORE,
-                   'body': table_features}
+    def test_pack_unpack_desc(self):
+        """Testing multipart_type with OFPMP_DESC."""
+        options = TestMultipartRequest.get_attributes(
+            multipart_type=MultipartTypes.OFPMP_DESC)
+        self._test_pack_unpack(**options)
+
+    def test_pack_unpack_table(self):
+        """Testing multipart_type with OFPMP_TABLE."""
+        options = TestMultipartRequest.get_attributes(
+            multipart_type=MultipartTypes.OFPMP_TABLE)
+        self._test_pack_unpack(**options)
+
+    def test_pack_unpack__port_stats_request(self):
+        """Testing multipart_type with OFPMP_PORT_STATS."""
+        options = TestMultipartRequest.get_attributes(
+            multipart_type=MultipartTypes.OFPMP_PORT_STATS,
+            body=PortStatsRequest(port_no=33))
+        self._test_pack_unpack(**options)
+
+    def test_pack_unpack_port_desc(self):
+        """Testing multipart_type with OFPMP_PORT_DESC."""
+        options = TestMultipartRequest.get_attributes(
+            multipart_type=MultipartTypes.OFPMP_PORT_DESC)
+        self._test_pack_unpack(**options)
+
+    def test_pack_unpack_table_features(self):
+        """Testing multipart_type with OFPMP_TABLE_FEATURES."""
+        instance = [TableFeatures(table_id=2),
+                    TableFeatures(table_id=6),
+                    TableFeatures(table_id=4)]
+        options = TestMultipartRequest.get_attributes(
+            multipart_type=MultipartTypes.OFPMP_TABLE_FEATURES,
+            body=instance)
         self._test_pack_unpack(**options)
