1c8accc402babe3f810dda273f7da06bba87c8f5
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-03-28 14:49:08 -0300

    Fixing package version
    FIX #332

--- pyof/__init__.py
+++ pyof/__init__.py
@@ -3,4 +3,4 @@
 This package is a library that parses and creates OpenFlow Messages.
 It contains all implemented versions of OpenFlow protocol
 """
-__version__ = '1.1.0b1.dev0'
+__version__ = '2017.1b1'
