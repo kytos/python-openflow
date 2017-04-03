971b5a512a50216e0b80a3d1762318db8b53a5f2
Artur Baruchi <artur.baruchi@gmail.com>
2017-04-03 13:50:50 -0300

    Adds binary data field to echo_reply and echo_request messages - Issue #136

--- pyof/v0x01/symmetric/echo_reply.py
+++ pyof/v0x01/symmetric/echo_reply.py
@@ -5,6 +5,7 @@
 # Third-party imports
 
 from pyof.foundation.base import GenericMessage
+from pyof.foundation.basic_types import BinaryData
 from pyof.v0x01.common.header import Header, Type
 
 __all__ = ('EchoReply',)
@@ -19,11 +20,13 @@ class EchoReply(GenericMessage):
     """
 
     header = Header(message_type=Type.OFPT_ECHO_REPLY, length=8)
+    data = BinaryData()
 
-    def __init__(self, xid=None):
+    def __init__(self, xid=None, data=b''):
         """The constructor takes the parameters below.
 
         Args:
             xid (int): xid to be used on the message header.
         """
         super().__init__(xid)
+        self.data = data
