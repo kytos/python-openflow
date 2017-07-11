5600802256f307c0da645c2cba5390bae298e284
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 14:26:01 -0300

    apply new test implementation for queue stats request

--- tests/v0x01/test_controller2switch/test_queue_stats_request.py
+++ tests/v0x01/test_controller2switch/test_queue_stats_request.py
@@ -1,20 +1,15 @@
 """Test for QueueStatsRequest message."""
 from pyof.v0x01.controller2switch.common import QueueStatsRequest, StatsTypes
 from pyof.v0x01.controller2switch.stats_request import StatsRequest
-from tests.test_struct import TestStruct
+from tests.test_struct import TestMsgDumpFile
 
 
-class TestQueueStatsRequest(TestStruct):
+class TestQueueStatsRequest(TestMsgDumpFile):
     """Test for QueueStatsRequest message."""
 
-    @classmethod
-    def setUpClass(cls):
-        """[Controller2Switch/QueueStatsRequest] - size 8."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x01', 'ofpt_queue_stats_request')
-        super().set_raw_dump_object(StatsRequest, xid=14,
-                                    body_type=StatsTypes.OFPST_QUEUE,
-                                    flags=0,
-                                    body=QueueStatsRequest(port_no=80,
-                                                           queue_id=5))
-        super().set_minimum_size(12)
+    dumpfile = 'v0x01/ofpt_queue_stats_request.dat'
+    obj = StatsRequest(xid=14, body_type=StatsTypes.OFPST_QUEUE,
+                       flags=0,
+                       body=QueueStatsRequest(port_no=80,
+                                              queue_id=5))
+    min_size = 12
