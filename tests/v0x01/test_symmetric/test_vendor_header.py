897653b5027042bce1d8392944638b448786049d
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 14:37:56 -0300

    apply new test implementation for vendor header

--- tests/v0x01/test_symmetric/test_vendor_header.py
+++ tests/v0x01/test_symmetric/test_vendor_header.py
@@ -1,15 +1,11 @@
 """Testing VendorHeader message."""
 from pyof.v0x01.symmetric.vendor_header import VendorHeader
-from tests.test_struct import TestStruct
+from tests.test_struct import TestMsgDumpFile
 
 
-class TestVendorHeader(TestStruct):
+class TestVendorHeader(TestMsgDumpFile):
     """Vendor message tests (also those in :class:`.TestDump`)."""
 
-    @classmethod
-    def setUpClass(cls):
-        """Configure raw file and its object in parent class (TestDump)."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x01', 'ofpt_vendor_header')
-        super().set_raw_dump_object(VendorHeader, xid=4, vendor=128)
-        super().set_minimum_size(12)
+    dumpfile = 'v0x01/ofpt_vendor_header.dat'
+    obj = VendorHeader(xid=4, vendor=128)
+    min_size = 12
