4190b19b8c8b22e9f626b2ee4f6fe899afb9a5a6
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 13:29:04 -0300

    apply new test implementation for BarrierReply class

--- tests/v0x01/test_controller2switch/test_barrier_reply.py
+++ tests/v0x01/test_controller2switch/test_barrier_reply.py
@@ -1,15 +1,11 @@
 """Barrier reply message tests."""
 from pyof.v0x01.controller2switch.barrier_reply import BarrierReply
-from tests.test_struct import TestStruct
+from tests.test_struct import TestMsgDumpFile
 
 
-class TestBarrierReply(TestStruct):
-    """Barrier reply message tests (also those in :class:`.TestDump`)."""
+class TestBarrierReply(TestMsgDumpFile):
+    """Barrier reply message tests."""
 
-    @classmethod
-    def setUpClass(cls):
-        """Configure raw file and its object in parent class (TestDump)."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x01', 'ofpt_barrier_reply')
-        super().set_raw_dump_object(BarrierReply, xid=5)
-        super().set_minimum_size(8)
+    dumpfile = 'v0x01/ofpt_barrier_reply.dat'
+    obj = BarrierReply(xid=5)
+    min_size = 8
