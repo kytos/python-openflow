d4be345c1292964388f56a0f20a2735fed950cd7
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 14:07:15 -0300

    apply new test implementation for flow stats request

--- tests/v0x01/test_controller2switch/test_flow_stats_request.py
+++ tests/v0x01/test_controller2switch/test_flow_stats_request.py
@@ -2,32 +2,23 @@
 from pyof.v0x01.common.flow_match import Match
 from pyof.v0x01.controller2switch.common import FlowStatsRequest, StatsTypes
 from pyof.v0x01.controller2switch.stats_request import StatsRequest
-from tests.test_struct import TestStruct
+from tests.test_struct import TestMsgDumpFile
 
 
-class TestFlowStatsRequest(TestStruct):
+class TestFlowStatsRequest(TestMsgDumpFile):
     """Test class for TestFlowStatsRequest."""
 
-    @classmethod
-    def setUpClass(cls):
-        """[Controller2Switch/FlowStatsRequest] - size 44."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x01', 'ofpt_flow_stats_request')
-        super().set_raw_dump_object(StatsRequest, xid=12,
-                                    body_type=StatsTypes.OFPST_FLOW,
-                                    flags=0, body=_get_flow_stats_request())
-        super().set_minimum_size(12)
+    dumpfile = 'v0x01/ofpt_flow_stats_request.dat'
 
+    match = Match(in_port=80, dl_src='01:02:03:04:05:06',
+                  dl_dst='01:02:03:04:05:06', dl_vlan=1,
+                  dl_vlan_pcp=1, dl_type=1,
+                  nw_tos=1, nw_proto=1,
+                  nw_src='192.168.0.1', nw_dst='192.168.0.1',
+                  tp_src=80, tp_dst=80)
+    flow_sr = FlowStatsRequest(match=match, table_id=1, out_port=80)
+    obj = StatsRequest(xid=12,
+                       body_type=StatsTypes.OFPST_FLOW,
+                       flags=0, body=flow_sr)
 
-def _get_flow_stats_request():
-    return FlowStatsRequest(match=_get_match(), table_id=1, out_port=80)
-
-
-def _get_match():
-    """Function used to return a Match instance."""
-    return Match(in_port=80, dl_src='01:02:03:04:05:06',
-                 dl_dst='01:02:03:04:05:06', dl_vlan=1,
-                 dl_vlan_pcp=1, dl_type=1,
-                 nw_tos=1, nw_proto=1,
-                 nw_src='192.168.0.1', nw_dst='192.168.0.1',
-                 tp_src=80, tp_dst=80)
+    min_size = 12
