7accf0a1a3c3c388ebaafee14acfe29668be8bac
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 11:16:09 -0300

    apply new test implementation for PortStatus

--- tests/v0x01/test_asynchronous/test_port_status.py
+++ tests/v0x01/test_asynchronous/test_port_status.py
@@ -4,31 +4,37 @@ from pyof.foundation.constants import OFP_MAX_PORT_NAME_LEN
 from pyof.v0x01.asynchronous.port_status import PortReason, PortStatus
 from pyof.v0x01.common.phy_port import (
     PhyPort, PortConfig, PortFeatures, PortState)
-from tests.test_struct import TestStruct
+from tests.test_struct import TestMsgDumpFile, TestMsgDump
 
 
-class TestPortStatus(TestStruct):
-    """Test the Port Status message."""
+class TestPortStatus_1(TestMsgDumpFile):
+    """Test the PortStatus class."""
 
-    @classmethod
-    def setUpClass(cls):
-        """Configure raw file and its object in parent class (TestDump)."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x01', 'ofpt_port_status')
-        super().set_raw_dump_object(_new_portstatus)
-        super().set_minimum_size(64)
+    dumpfile = 'v0x01/ofpt_port_status.dat'
 
-    def test_pack(self):
-        """Skip pack test for now."""
-        self.skipTest('Need to recover dump contents.')
+    desc_name = 's1-eth1'
+    desc = PhyPort(port_no=1,
+                   hw_addr=HWAddress('9a:da:11:8a:f4:0c'),
+                   name=desc_name,
+                   config=0,
+                   state=0,
+                   curr=192,
+                   advertised=0,
+                   supported=0,
+                   peer=0)
+    obj = PortStatus(xid=0,
+                     reason=PortReason.OFPPR_MODIFY,
+                     desc=desc)
 
-    def test_unpack(self):
-        """Skipt unpack test for now."""
-        self.skipTest('Need to recover dump contents.')
 
+class TestPortStatus_2(TestMsgDump):
+    """Test the PortStatus class."""
 
-def _new_portstatus():
-    """Crate new PortStatus and PhyPort instances."""
+    dump = b'\x01\x0c\x00@\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'
+    dump += b'\x00\x00\x02\n\n\x12\x12\x10\x10XXXXXXXXXXXXXXX\x00\x00\x00'
+    dump += b'\x00\x02\x00\x00\x02\x00\x00\x00\x00@\x00\x00\x04\x00\x00'
+    dump += b'\x00\x02\x00\x00\x00\x02\x00'  # needs to be checked
+
     desc_name = 'X' * OFP_MAX_PORT_NAME_LEN
     desc = PhyPort(port_no=2,
                    hw_addr=HWAddress('0a:0a:12:12:10:10'),
@@ -39,6 +45,6 @@ def _new_portstatus():
                    advertised=PortFeatures.OFPPF_PAUSE,
                    supported=PortFeatures.OFPPF_AUTONEG,
                    peer=PortFeatures.OFPPF_AUTONEG)
-    return PortStatus(xid=1,
-                      reason=PortReason.OFPPR_ADD,
-                      desc=desc)
+    obj = PortStatus(xid=1,
+                     reason=PortReason.OFPPR_ADD,
+                     desc=desc)
