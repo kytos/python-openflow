988d26806b6e69d60b002b29cef86bef9e37c857
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 14:27:02 -0300

    apply new test implementation for set config

--- tests/v0x01/test_controller2switch/test_set_config.py
+++ tests/v0x01/test_controller2switch/test_set_config.py
@@ -1,18 +1,13 @@
 """Set Config message tests."""
 from pyof.v0x01.controller2switch.common import ConfigFlags
 from pyof.v0x01.controller2switch.set_config import SetConfig
-from tests.test_struct import TestStruct
+from tests.test_struct import TestMsgDumpFile
 
 
-class TestSetConfig(TestStruct):
+class TestSetConfig(TestMsgDumpFile):
     """Test the Set Config message."""
 
-    @classmethod
-    def setUpClass(cls):
-        """Configure raw file and its object in parent class (TestDump)."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x01', 'ofpt_set_config')
-        super().set_raw_dump_object(SetConfig, xid=3,
-                                    flags=ConfigFlags.OFPC_FRAG_NORMAL,
-                                    miss_send_len=128)
-        super().set_minimum_size(12)
+    dumpfile = 'v0x01/ofpt_set_config.dat'
+    obj = SetConfig(xid=3, flags=ConfigFlags.OFPC_FRAG_NORMAL,
+                    miss_send_len=128)
+    min_size = 12
