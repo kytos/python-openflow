11d34b3ed35f0e13cc8633fd236b76c6b2f87d11
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 14:23:16 -0300

    apply new test implementation for get config request

--- tests/v0x01/test_controller2switch/test_get_config_request.py
+++ tests/v0x01/test_controller2switch/test_get_config_request.py
@@ -3,7 +3,7 @@ from pyof.v0x01.controller2switch.get_config_request import GetConfigRequest
 from tests.test_struct import TestMsgDumpFile
 
 
-class TestGetConfigReply(TestMsgDumpFile):
+class TestGetConfigRequest(TestMsgDumpFile):
     """Test class for TestGetConfigReply."""
 
     dumpfile = 'v0x01/ofpt_get_config_request.dat'
