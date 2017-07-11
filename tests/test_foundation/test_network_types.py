e20298ea918537f5452df2ad1bf845b2833b8287
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 10:58:45 -0300

    apply new test implementation for network types

--- tests/test_foundation/test_network_types.py
+++ tests/test_foundation/test_network_types.py
@@ -3,6 +3,14 @@ import unittest
 
 from pyof.foundation.basic_types import BinaryData
 from pyof.foundation.network_types import GenericTLV, IPv4
+from tests.test_struct import TestStructDump
+
+
+class TestGenericTLV(TestStructDump):
+    """Test the GenericTLV value data."""
+
+    dump = b'\xfe\x04test'
+    obj = GenericTLV(value=BinaryData(b'test'))
 
 
 class TestNetworkTypes(unittest.TestCase):
@@ -15,6 +23,16 @@ class TestNetworkTypes(unittest.TestCase):
         tlv_unpacked = GenericTLV()
         tlv_unpacked.unpack(tlv.pack())
         self.assertEqual(tlv.value.value, tlv_unpacked.value.value)
+
+
+class TestIPV4(TestStructDump):
+    """Test the IPV4 class."""
+
+    dump = b'F(\x00 \x00\x00\x00\x00@\x11\x02' +\
+        b'\xc5\xc0\xa8\x00\n\xac\x10\n\x1e1000testdata'
+    obj = IPv4(dscp=10, ttl=64, protocol=17, source="192.168.0.10",
+               destination="172.16.10.30", options=b'1000',
+               data=b'testdata')
 
 
 class TestIPv4(unittest.TestCase):
