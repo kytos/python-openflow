db73a7d1dcad29a33425dbaba57b1badd4812047
Erick Vermot <erick.vermot@gmail.com>
2017-07-11 14:08:27 -0300

    apply new test implementation for get config reply

--- tests/v0x01/test_controller2switch/test_get_config_reply.py
+++ tests/v0x01/test_controller2switch/test_get_config_reply.py
@@ -1,18 +1,14 @@
 """Test GetConfigReply message."""
 from pyof.v0x01.controller2switch.common import ConfigFlags
 from pyof.v0x01.controller2switch.get_config_reply import GetConfigReply
-from tests.test_struct import TestStruct
+from tests.test_struct import TestMsgDumpFile
 
 
-class TestGetConfigReply(TestStruct):
+class TestGetConfigReply(TestMsgDumpFile):
     """Test class for TestGetConfigReply."""
 
-    @classmethod
-    def setUpClass(cls):
-        """[Controller2Switch/GetConfigReply] - size 12."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x01', 'ofpt_get_config_reply')
-        super().set_raw_dump_object(GetConfigReply, xid=13,
-                                    flags=ConfigFlags.OFPC_FRAG_REASM,
-                                    miss_send_len=1024)
-        super().set_minimum_size(12)
+    dumpfile = 'v0x01/ofpt_get_config_reply.dat'
+    obj = GetConfigReply(xid=13,
+                         flags=ConfigFlags.OFPC_FRAG_REASM,
+                         miss_send_len=1024)
+    min_size = 12
