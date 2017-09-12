1fecb55f5cae2ba76e08a4debb05237e0852df28
Macartur Sousa <macartur.sc@gmail.com>
2017-09-12 12:21:06 -0300

    Minor fixes in pyof/utils.py and add tests
    Fix #381

--- pyof/utils.py
+++ pyof/utils.py
@@ -1,16 +1,21 @@
-"""General Unpack utils for python-openflow."""
-import pyof.v0x01.common.header
-import pyof.v0x01.common.utils
-import pyof.v0x04.common.header
-import pyof.v0x04.common.utils
+"""General Unpack utils for python-openflow.
+
+This package was moved from kytos/of_core for the purpose of creating a generic
+method to perform package unpack independent of the OpenFlow version.
+"""
+from pyof import v0x01, v0x04
 from pyof.foundation.exceptions import UnpackException
 
-pyof_version_libs = {0x01: pyof.v0x01,
-                     0x04: pyof.v0x04}
+PYOF_VERSION_LIBS = {0x01: v0x01,
+                     0x04: v0x04}
 
 
 def validate_packet(packet):
-    """Check if packet is valid OF packet."""
+    """Check if packet is valid OF packet.
+
+    Raises:
+        UnpackException: If the packet is invalid.
+    """
     if not isinstance(packet, bytes):
         raise UnpackException('invalid packet')
 
@@ -29,22 +34,32 @@ def validate_packet(packet):
 
 
 def unpack(packet):
-    """Unpack the OpenFlow Packet and returns a message."""
+    """Unpack the OpenFlow Packet and returns a message.
+
+    Returns:
+        GenericMessage: Message unpacked based on openflow packet.
+
+    Raises:
+        UnpackException: if the packet can't be unpacked.
+    """
     validate_packet(packet)
 
     version = packet[0]
     try:
-        pyof_lib = pyof_version_libs[version]
+        pyof_lib = PYOF_VERSION_LIBS[version]
     except KeyError:
         raise UnpackException('Version not supported')
 
     try:
         header = pyof_lib.common.header.Header()
-        header.unpack(packet[:8])
+        header.unpack(packet[:header.get_size()])
         message = pyof_lib.common.utils.new_message_from_header(header)
-        binary_data = packet[8:]
-        if binary_data:
+
+        binary_data = packet[header.get_size():]
+        binary_data_size = header.length - header.get_size()
+
+        if binary_data and len(binary_data) == binary_data_size:
             message.unpack(binary_data)
         return message
-    except (UnpackException, ValueError) as e:
-        raise UnpackException(e)
+    except (UnpackException, ValueError) as exception:
+        raise UnpackException(exception)
