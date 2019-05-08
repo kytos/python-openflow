da312ff740b1726b698b148c5100a22bc9f15bdd
Roicxy Alonso Gonzalez <ralon051@fiu.edu>
2019-05-07 20:00:25 -0400

    Changing the indentation and creating spaces to the HelloElemVersionBitmap method

--- pyof/v0x05/symmetric/hello.py
+++ pyof/v0x05/symmetric/hello.py
@@ -135,6 +135,7 @@ class HelloElemVersionBitmap(HelloElemHeader):
     """ Version bitmap Hello Element
     There is not need to enter the Version bitmap because is entered automatically as the type of message
     This method is under review.  """
+
     # List of bitmaps - supported versions
     bitmaps = BinaryData()
 
