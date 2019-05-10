92d7ad16e5e7c006c9a43594662a4398a314d657
Roicxy Alonso Gonzalez <ralon051@fiu.edu>
2019-05-09 20:52:40 -0400

    Added the echo_reply file

---
+++ pyof/v0x05/symmetric/echo_reply.py
@@ -0,0 +1,34 @@
+"""Defines Echo Reply message during the handshake."""
+
+# System imports
+
+# Third-party imports
+
+from pyof.foundation.base import GenericMessage
+from pyof.foundation.basic_types import BinaryData
+from pyof.v0x05.common.header import Header, Type
+
+__all__ = ('EchoReply',)
+
+# Classes
+
+
+class EchoReply(GenericMessage):
+    """OpenFlow Reply message.
+
+    This message does not contain a body beyond the OpenFlow Header.
+    """
+
+    header = Header(message_type=Type.OFPT_ECHO_REPLY, length=8)
+
+    data = BinaryData()
+
+    def __init__(self, xid=None, data=b''):
+        """Create a EchoReply with the optional parameters below.
+
+        Args:
+            xid (int): xid to be used on the message header.
+            data (bytes): arbitrary-length data field.
+        """
+        super().__init__(xid)
+        self.data = data
