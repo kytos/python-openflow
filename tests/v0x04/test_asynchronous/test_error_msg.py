63034d1205cb8a8eacc8d2dbfccc8eeaf4f6917b
Gleyberson Andrade <gleybersonandrade@gmail.com>
2019-09-03 10:11:15 -0300

    Create some raw files and of_1.3 tests
    Create tests from Hello, Packet Out, Flow Mod, Set Config, Flow Stats, Aggregate Stats, Port Desc, Group Stats, Error, Meter Multipart Request and Packet In messages

--- tests/v0x04/test_asynchronous/test_error_msg.py
+++ tests/v0x04/test_asynchronous/test_error_msg.py
@@ -1,5 +1,7 @@
 """Testing v0x04 error message class."""
-from pyof.v0x04.asynchronous.error_msg import ErrorMsg
+from pyof.foundation.exceptions import MethodNotImplemented
+from pyof.v0x04.asynchronous.error_msg import (
+    ErrorExperimenterMsg, ErrorMsg, ErrorType, MeterModFailedCode)
 from tests.test_struct import TestStruct
 
 
@@ -9,7 +11,26 @@ class TestErrorMsg(TestStruct):
     @classmethod
     def setUpClass(cls):
         """Configure raw file and its object in parent class (TestDump)."""
+        error_type = ErrorType.OFPET_METER_MOD_FAILED
+        code = MeterModFailedCode.OFPMMFC_UNKNOWN_METER
         super().setUpClass()
-        super().set_raw_dump_file('v0x04', 'ofpt_error_msg')
-        super().set_raw_dump_object(ErrorMsg, xid=1, error_type=1, code=1)
+        super().set_raw_dump_file('v0x04', 'ofpt_error')
+        super().set_raw_dump_object(ErrorMsg, xid=1, error_type=error_type,
+                                    code=code, data=_get_data())
         super().set_minimum_size(12)
+
+    @staticmethod
+    def test_unpack():
+        """[Asynchronous/error_msg] - unpack ErrorExperimenterMsg."""
+        unpacked = ErrorExperimenterMsg()
+        try:
+            unpacked.unpack("pack")
+        except MethodNotImplemented:
+            pass
+
+
+def _get_data():
+    """Return data for ErrorMsg object."""
+    data = b'\x04\x12\x00\x18\x00\x00\x00\x01\x00\x0a\x00\x00\x00\x00\x00'
+    data += b'\x00\x00\x00\x00\x01\x00\x00\x00\x00'
+    return data
---
+++ tests/v0x04/test_controller2switch/test_aggregate_stats.py
@@ -0,0 +1,26 @@
+"""Aggregate stats request message."""
+from pyof.v0x04.controller2switch.common import MultipartType
+from pyof.v0x04.controller2switch.multipart_reply import (
+    AggregateStatsReply, MultipartReply)
+from tests.test_struct import TestStruct
+
+
+class TestAggregateStats(TestStruct):
+    """Aggregate stats message."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Configure raw file and its object in parent class (TestDump)."""
+        mp_type = MultipartType.OFPMP_AGGREGATE
+        super().setUpClass()
+        super().set_raw_dump_file('v0x04', 'ofpt_aggregate_stats')
+        super().set_raw_dump_object(MultipartReply, xid=1,
+                                    multipart_type=mp_type,
+                                    flags=0,
+                                    body=_get_body())
+        super().set_minimum_size(16)
+
+
+def _get_body():
+    """Return the body used by MultipartReply message."""
+    return AggregateStatsReply(packet_count=2, byte_count=220, flow_count=2)
