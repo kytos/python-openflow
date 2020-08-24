34072711c828a797f70c1251de15ad4109dc534d
Gleyberson Andrade <gleybersonandrade@gmail.com>
2020-08-24 10:36:35 -0300

    Fix Travis false positive message
    Fix Coverage and Test not throwing errors when tests fail.
    
    Co-Authored-By: Carlos Magno <cmagnobarbosa@gmail.com>

--- setup.py
+++ setup.py
@@ -111,6 +111,8 @@ class Test(TestCommand):
             check_call(cmd, shell=True)
         except CalledProcessError as exc:
             print(exc)
+            print('Unit tests failed. Fix the error(s) above and try again.')
+            sys.exit(-1)
 
 
 class TestCoverage(Test):
@@ -126,6 +128,8 @@ class TestCoverage(Test):
             check_call(cmd, shell=True)
         except CalledProcessError as exc:
             print(exc)
+            print('Coverage tests failed. Fix the errors above and try again.')
+            sys.exit(-1)
 
 
 class DocTest(SimpleCommand):
