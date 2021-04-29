30e4a09c83b0ee2e0c7512370a18a4b21b3b3eac
Carlos Magno <cmagnobarbosa@gmail.com>
2021-04-29 18:01:47 -0300

    Version bump to 2021.final

--- pyof/__init__.py
+++ pyof/__init__.py
@@ -3,4 +3,4 @@
 This package is a library that parses and creates OpenFlow Messages.
 It contains all implemented versions of OpenFlow protocol
 """
-__version__ = '2020.2'
+__version__ = '2021.final'
